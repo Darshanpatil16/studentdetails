d1deb8c799cb8ce4c58d744a0b21f72b852ae0a1
Darshanpatil16 <darshan16062006@gmail.com>
2025-09-29 17:16:05 +0530

    save to main

--- studentdetails.py
+++ studentdetails.py
@@ -5,3 +5,5 @@ collage=input("Enter College Name")
 
 print(f"Student Details: Name is {name}, USN is {usn}, Course is {course}, Collage name is {collage}")
 print("The details")
+print("----------------")
+print("----------------")
