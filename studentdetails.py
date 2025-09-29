478367c52a61c592929c7dddb3254f74da7a8e2f
Darshanpatil16 <darshan16062006@gmail.com>
2025-09-29 17:23:48 +0530

    saving to master

--- studentdetails.py
+++ studentdetails.py
@@ -3,3 +3,7 @@ usn = input("Enter your usn: ")
 course = input("Enter your course: ")
 
 print(f"Student Details: Name is {name}, USN is {usn}, Course is {course}")
+print("----------------")
+print("----------------")
+
+
